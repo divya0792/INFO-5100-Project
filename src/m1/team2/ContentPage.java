713fb916671fdeeb4c9de7cdda6d992af9310879
wimp666 <qwy19960215@gmail.com>
2019-12-01 18:16:23 -0800

    Update ContentPage.java

--- src/m1/team2/ContentPage.java
+++ src/m1/team2/ContentPage.java
@@ -10,6 +10,7 @@ import javax.swing.JTextField;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.SwingConstants;
@@ -17,6 +18,8 @@ import javax.swing.JTextPane;
 import javax.swing.JComboBox;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.LineBorder;
+
+
 import java.awt.Color;
 import javax.swing.JCheckBox;
 import javax.swing.DefaultComboBoxModel;
@@ -47,9 +50,9 @@ public class ContentPage extends JFrame {
 	
 	private JTextPane txtpnPlaintext, txtpnHeaderData, txtpnFootenData, txtpnContent_2, txtpnContent_1;
 
-	private JPanel panel, panel_1, panel_2, panel_3, panel_4, panel_5, panel_6, panel_7, panel_8;
+	private JPanel panel_ButtonArea, panel_Content_1, panel_Content_2, panel_FontArea, panel_Header, panel_Footen, panel_Center, panel_PreviewArea, panel_CheckBoxArea;
 
-	private JComboBox comboBox, comboBox_1, comboBox_5, comboBox_4;
+	private JComboBox comboBox_Size, comboBox_Color, comboBox_Position, comboBox_BackGroundColor;
 
 	private JLabel lblNewLabel, lblNewLabel_1, lblNewLabel_2, lblNewLabel_4, lblNewLabel_5;
 
@@ -68,100 +71,100 @@ public class ContentPage extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		newPanel_4();	
-		newPanel_1();
-		newPanel_2();
-		newPanel_5();
-		newPanel_8();
-		newComboBox_5();
-		newPanel_3();
-		newPanel_7();
-		newPanel();
-		newPanel_6();
+		newHeaderPanel();	
+		newContent_1Panel();
+		newContent_2Panel();
+		newFootenPanel();
+		newCheckBoxAreaPanel();
+		newPositionComboBox();
+		newFontAreaPanel();
+		newPreviewAreaPanel();
+		newButtonAreaPanel();
+		newCenterPanel();
 		
 	}
 	
-	private void newPanel_4() {
-		panel_4 = new JPanel();
+	private void newHeaderPanel() {
+		panel_Header = new JPanel();
 		txtpnHeaderData = new JTextPane();
 		
-		panel_4.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
-		contentPane.add(panel_4, BorderLayout.NORTH);
-		panel_4.setLayout(new GridLayout(1, 2, 0, 0));
+		panel_Header.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		contentPane.add(panel_Header, BorderLayout.NORTH);
+		panel_Header.setLayout(new GridLayout(1, 2, 0, 0));
 		txtpnHeaderData.setText("Header Data : ");
-		panel_4.add(txtpnHeaderData);
+		panel_Header.add(txtpnHeaderData);
 	}
-	private void newPanel_1() {
-		panel_1 = new JPanel();
+	private void newContent_1Panel() {
+		panel_Content_1 = new JPanel();
 		txtpnContent_1 = new JTextPane();
 		
-		panel_1.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
-		contentPane.add(panel_1, BorderLayout.WEST);
-		panel_1.setLayout(new GridLayout(0, 1, 0, 0));		
+		panel_Content_1.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		contentPane.add(panel_Content_1, BorderLayout.WEST);
+		panel_Content_1.setLayout(new GridLayout(0, 1, 0, 0));		
 		txtpnContent_1.setText("Content 1: ");
-		panel_1.add(txtpnContent_1);
+		panel_Content_1.add(txtpnContent_1);
 	}
-	private void newPanel_2() {
-		panel_2 = new JPanel();
+	private void newContent_2Panel() {
+		panel_Content_2 = new JPanel();
 		txtpnContent_2 = new JTextPane();
 		
-		panel_2.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
-		contentPane.add(panel_2, BorderLayout.EAST);
-		panel_2.setLayout(new GridLayout(0, 1, 0, 0));
+		panel_Content_2.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		contentPane.add(panel_Content_2, BorderLayout.EAST);
+		panel_Content_2.setLayout(new GridLayout(0, 1, 0, 0));
 		txtpnContent_2.setText("Content 2: ");
-		panel_2.add(txtpnContent_2);
+		panel_Content_2.add(txtpnContent_2);
 	}
-	private void newPanel_5() {
-		panel_5 = new JPanel();
+	private void newFootenPanel() {
+		panel_Footen = new JPanel();
 		txtpnFootenData = new JTextPane();
 		
-		panel_5.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
-		contentPane.add(panel_5, BorderLayout.SOUTH);
-		panel_5.setLayout(new GridLayout(0, 1, 0, 0));
+		panel_Footen.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		contentPane.add(panel_Footen, BorderLayout.SOUTH);
+		panel_Footen.setLayout(new GridLayout(0, 1, 0, 0));
 		txtpnFootenData.setText("Footen Data : ");
-		panel_5.add(txtpnFootenData);
+		panel_Footen.add(txtpnFootenData);
 	}
-	private void newPanel_3() {
-		comboBox_4 = new JComboBox();
-		comboBox = new JComboBox();
-		comboBox_1 = new JComboBox();
+	private void newFontAreaPanel() {
+		comboBox_BackGroundColor = new JComboBox();
+		comboBox_Size = new JComboBox();
+		comboBox_Color = new JComboBox();
 		lblNewLabel_4 = new JLabel("Position");
 		lblNewLabel = new JLabel("Font Size");
 		lblNewLabel_2 = new JLabel("Format");
 		lblNewLabel_5 = new JLabel("Font Color");
 		lblNewLabel_1 = new JLabel("Background Color");
 		
-		panel_3 = new JPanel();
-		panel_3.setLayout(new GridLayout(5, 2, 0, 0));
+		panel_FontArea = new JPanel();
+		panel_FontArea.setLayout(new GridLayout(5, 2, 0, 0));
 		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_3.add(lblNewLabel_4);	
-		panel_3.add(comboBox_5);
+		panel_FontArea.add(lblNewLabel_4);	
+		panel_FontArea.add(comboBox_Position);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_3.add(lblNewLabel);
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"9", "9.5", "10", "10.5", "11", "11.5", "12"}));
-		panel_3.add(comboBox);
-		panel_3.add(lblNewLabel_2);
+		panel_FontArea.add(lblNewLabel);
+		comboBox_Size.setModel(new DefaultComboBoxModel(new String[] {"9", "10", "11", "12", "13"}));
+		panel_FontArea.add(comboBox_Size);
+		panel_FontArea.add(lblNewLabel_2);
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_3.add(panel_8);
+		panel_FontArea.add(panel_CheckBoxArea);
 		lblNewLabel_5.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_3.add(lblNewLabel_5);
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"red", "black", "white", "green", "blue"}));
-		panel_3.add(comboBox_1);
+		panel_FontArea.add(lblNewLabel_5);
+		comboBox_Color.setModel(new DefaultComboBoxModel(new String[] {"red", "black", "white", "green", "blue"}));
+		panel_FontArea.add(comboBox_Color);
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_3.add(lblNewLabel_1);
-		comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"black", "white", "grey"}));
-		panel_3.add(comboBox_4);
+		panel_FontArea.add(lblNewLabel_1);
+		comboBox_BackGroundColor.setModel(new DefaultComboBoxModel(new String[] {"black", "white", "grey"}));
+		panel_FontArea.add(comboBox_BackGroundColor);
 	}
-	private void newPanel_7() {
-		panel_7 = new JPanel();
+	private void newPreviewAreaPanel() {
+		panel_PreviewArea = new JPanel();
 		txtpnPlaintext = new JTextPane();
 		btnSubmit = new JButton("Submit");
 		btnPreview = new JButton("Preview");
 		btnReset = new JButton("Reset");
 		
-		panel_7.setLayout(new BorderLayout(0, 0));
-		setTxtPaneText(txtpnPlaintext, txtpnHeaderData.getText());
-		panel_7.add(txtpnPlaintext, BorderLayout.CENTER);
+		panel_PreviewArea.setLayout(new BorderLayout(0, 0));
+		setTxtPaneText(txtpnPlaintext, txtpnHeaderData.getText(), txtpnHeaderData.getFont().isBold(), txtpnHeaderData.getFont().isItalic(), txtpnHeaderData.getFont().getSize() );
+		panel_PreviewArea.add(txtpnPlaintext, BorderLayout.CENTER);
 		btnSubmit.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -177,56 +180,65 @@ public class ContentPage extends JFrame {
 			}
 		});
 	}
-	private void newPanel() {
-		panel = new JPanel();
-		panel.add(btnPreview);
-		panel.add(btnReset);
-		panel.add(btnSubmit);
+	private void newButtonAreaPanel() {
+		panel_ButtonArea = new JPanel();
+		panel_ButtonArea.add(btnPreview);
+		panel_ButtonArea.add(btnReset);
+		panel_ButtonArea.add(btnSubmit);
 	}
-	private void newPanel_6() {
-		panel_6 = new JPanel();
-		panel_6.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
-		contentPane.add(panel_6, BorderLayout.CENTER);
-		panel_6.setLayout(new BorderLayout(0, 0));
-		panel_6.add(panel_3, BorderLayout.NORTH);
-		panel_6.add(panel, BorderLayout.SOUTH);
-		panel_6.add(panel_7, BorderLayout.CENTER);
+	private void newCenterPanel() {
+		panel_Center = new JPanel();
+		panel_Center.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		contentPane.add(panel_Center, BorderLayout.CENTER);
+		panel_Center.setLayout(new BorderLayout(0, 0));
+		panel_Center.add(panel_FontArea, BorderLayout.NORTH);
+		panel_Center.add(panel_ButtonArea, BorderLayout.SOUTH);
+		panel_Center.add(panel_PreviewArea, BorderLayout.CENTER);
 	}
-	private void newPanel_8() {
-		panel_8 = new JPanel();
-		panel_8.setLayout(new GridLayout(0, 2, 0, 0));
+	private void newCheckBoxAreaPanel() {
+		panel_CheckBoxArea = new JPanel();
+		panel_CheckBoxArea.setLayout(new GridLayout(0, 2, 0, 0));
 		JCheckBox chckbxNewCheckBox = new JCheckBox("Is Bold");
-		panel_8.add(chckbxNewCheckBox);
+		panel_CheckBoxArea.add(chckbxNewCheckBox);
 		JCheckBox chckbxIsItalic = new JCheckBox("Is Italic");
-		panel_8.add(chckbxIsItalic);
+		panel_CheckBoxArea.add(chckbxIsItalic);
 	}
-	private void newComboBox_5() {
-		comboBox_5 = new JComboBox();
-		comboBox_5.addItemListener(new ItemListener() {
+	private void newPositionComboBox() {
+		comboBox_Position = new JComboBox();
+		comboBox_Position.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent e) {
 				switch (e.getStateChange())
 					{
 						case ItemEvent.SELECTED: 
 							String item = e.getItem().toString();
 							if(item.equals("Header Data")) {
-								setTxtPaneText(txtpnPlaintext, txtpnHeaderData.getText());
+								setTxtPaneText(txtpnPlaintext, txtpnHeaderData.getText(), txtpnHeaderData.getFont().isBold(), txtpnHeaderData.getFont().isItalic(), txtpnHeaderData.getFont().getSize());
 							}else if(item.equals("Content 1")) {
-								setTxtPaneText(txtpnPlaintext, txtpnContent_1.getText());
+								setTxtPaneText(txtpnPlaintext, txtpnContent_1.getText(), txtpnContent_1.getFont().isBold(), txtpnContent_1.getFont().isItalic(), txtpnContent_1.getFont().getSize());
 							}else if(item.equals("Content 2")) {
-								setTxtPaneText(txtpnPlaintext, txtpnContent_2.getText());
+								setTxtPaneText(txtpnPlaintext, txtpnContent_2.getText(), txtpnContent_2.getFont().isBold(), txtpnContent_2.getFont().isItalic(), txtpnContent_2.getFont().getSize());
 							}else if(item.equals("Footen Data")) {
-								setTxtPaneText(txtpnPlaintext, txtpnFootenData.getText());
+								setTxtPaneText(txtpnPlaintext, txtpnFootenData.getText(), txtpnFootenData.getFont().isBold(), txtpnFootenData.getFont().isItalic(), txtpnFootenData.getFont().getSize());
 							}
 						break;
 					}
 			}
 		});
-		comboBox_5.setModel(new DefaultComboBoxModel(new String[] {"Header Data", "Content 1", "Content 2", "Footen Data"}));
+		comboBox_Position.setModel(new DefaultComboBoxModel(new String[] {"Header Data", "Content 1", "Content 2", "Footen Data"}));
 		
 	}
 	
-	public void setTxtPaneText(JTextPane textPane, String text) {
+	public void setTxtPaneText(JTextPane textPane, String text, Boolean isBold, Boolean isItalic, int size) {
 		textPane.setText(text);
+		int temp = 0;		
+		if(isBold) {
+			temp += Font.BOLD;
+		}
+		if(isItalic) {
+			temp += Font.ITALIC;
+		}
+		Font f = new Font("Default", temp,size);
+		textPane.setFont(f);
 	}
 
 	
