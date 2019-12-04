90e0d6d5411b81e834e3478f770ce79bf24adbf9
Mingzhuo Yu <35823288+alex970504@users.noreply.github.com>
2019-12-03 21:23:19 -0800

    Add files via upload

--- src/m1/DBExecutor.java
+++ src/m1/DBExecutor.java
@@ -2,7 +2,10 @@ package m1;
 
 import dataproto.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class DBExecutor {
@@ -10,18 +13,42 @@ public class DBExecutor {
     // the only way to call api is like DBExecutor.INSTANCE.getContent();
     public static final DBExecutor INSTANCE = new DBExecutor();
 
+    private Map<String, List<RichText>> table;
+
     // prevent other class create instance
     private DBExecutor() {
         // init, connect DB
+        table = new HashMap<>();
+        RichText left = new RichText();
+        left.setPlainText("left");
+        RichText right = new RichText();
+        left.setPlainText("right");
+        RichText header = new RichText();
+        left.setPlainText("header");
+        RichText footer = new RichText();
+        left.setPlainText("footer");
+        List<RichText> row = new ArrayList<>();
+        row.add(header);
+        row.add(footer);
+        row.add(left);
+        row.add(right);
+        table.put("ch", row);
     }
- // [top, left, right, bottom]
+
     public List<RichText> getContent(Dealer dealer) {
-        // TODO
-        return null;
+        if (!table.containsKey(dealer.getId())) {
+            return null;
+        }
+        return table.get(dealer.getId());
     }
 
     public boolean submitContentChange(Dealer dealer, RichText header, RichText footer, RichText left, RichText right) {
-        // TODO
+        List<RichText> row = new ArrayList<>();
+        row.add(header);
+        row.add(footer);
+        row.add(left);
+        row.add(right);
+        table.put(dealer.getId(), row);
         return true;
     }
 }
