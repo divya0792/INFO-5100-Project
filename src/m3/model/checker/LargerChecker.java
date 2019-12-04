ec3a0336c733dba8b0aebdc47ed28b122d8f711f
GroovyEverywhere <55300195+GroovyEverywhere@users.noreply.github.com>
2019-12-03 21:37:29 -0800

    Add files via upload

---
+++ src/m3/model/checker/LargerChecker.java
@@ -0,0 +1,11 @@
+package m3.model.checker;
+
+public class LargerChecker extends Checker<Integer>{
+
+	@Override
+	public boolean check(Integer o1, Integer o2) {
+		// TODO Auto-generated method stub
+		return o1>o2;
+	}
+
+}
---
+++ src/m3/model/checker/LessChecker.java
@@ -0,0 +1,12 @@
+package m3.model.checker;
+
+public class LessChecker extends Checker <Integer>{
+
+	@Override
+	public boolean check(Integer o1, Integer o2) {
+		// TODO Auto-generated method stub
+		return o1<o2;
+	}
+	
+
+}
