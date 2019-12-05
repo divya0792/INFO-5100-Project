00ffa26997a177de40f61233f05a3d05b08f3a84
shuguo <ayagen@126.com>
2019-12-04 17:45:42 -0800

    mock a click action from lead list and pass a leadId as 17 to test

--- src/m4/Team3/Main.java
+++ src/m4/Team3/Main.java
@@ -1,9 +1,10 @@
 //package FinalProject;
 package m4.Team3;
+
 public class Main {
 	public static void main(String[] args) {	
 //		SubmitResponse window = new SubmitResponse();
-		SubmitResponseNew window = new SubmitResponseNew();
+		SubmitResponseNew window = new SubmitResponseNew("17");
 		window.frame.setVisible(true);		
 	}
 }
