c2141cf886666b2c41f893f3744220a3fe0505a7
Sahithi-Sarabu <55119648+Sahithi-Sarabu@users.noreply.github.com>
2019-12-05 15:43:44 -0800

    Update Main.java
    Creating instance for firstUI

--- src/m3/Main.java
+++ src/m3/Main.java
@@ -1,6 +1,11 @@
 package m3;
 
+import m3.view.FirstUI;
+import m3.view.SecondUI;
+
 public class Main {
   public static void main(String args[]){
+	  FirstUI fui = new FirstUI();
+	  fui.start();
   }
 }
