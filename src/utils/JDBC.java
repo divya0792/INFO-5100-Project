3582f401ef82ac62836132aaefe5006b0af77c52
shuguo <ayagen@126.com>
2019-12-04 17:43:22 -0800

    update conn info to real test env, and add two methods: getResults/update

--- src/utils/JDBC.java
+++ src/utils/JDBC.java
@@ -3,40 +3,64 @@ package utils;
 import java.sql.*;
 
 public class JDBC {
-  private Statement stmt;
-  private static JDBC _instance;
-  static public JDBC getInstance() {
-    if (_instance == null) {
-      _instance = new JDBC();
-    }
-    return _instance;
-  }
-  private JDBC() {
-    Connection conn = null;
-    stmt = null;
-    try{
-       //STEP 2: Register JDBC driver
-       Class.forName("com.mysql.jdbc.Driver");
+	private Statement stmt;
+	private static JDBC _instance;
 
-       //STEP 3: Open a connection
-       System.out.println("Connecting to a selected database...");
-       conn = DriverManager.getConnection(DB_URL, USER, PASS);
-       System.out.println("Connected database successfully...");
+	static public JDBC getInstance() {
+		if (_instance == null) {
+			_instance = new JDBC();
+		}
+		return _instance;
+	}
 
-       //STEP 4: Execute a query
-       System.out.println("Creating statement...");
-       this.stmt = conn.createStatement();
-      } catch(Exception e) {
+	private JDBC() {
+		Connection conn = null;
+		stmt = null;
+		try {
+			// STEP 2: Register JDBC driver
+			// Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
-    }
-  }
-  public ResultSet getResults(String query) {
-    ResultSet rs = null;
-    try {
-        //stmt is the connection statement
-        rs = this.stmt.executeQuery(sql);
-    } catch(Exception e) {
-    }
-    return rs;
-  }
+			// STEP 3: Open a connection
+			System.out.println("Connecting to a selected database...");
+			conn = DriverManager.getConnection(
+					"jdbc:sqlserver://is-swang01.ischool.uw.edu:1433;databaseName=VechileManagementSystem", "INFO6210",
+					"NEUHusky!");
+			System.out.println("Connected database successfully...");
+
+			// STEP 4: Execute a query
+			System.out.println("Creating statement...");
+			this.stmt = conn.createStatement();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public ResultSet getResults(String query) {
+		ResultSet rs = null;
+		try {
+			// stmt is the connection statement
+			System.out.println("select sql query: " + query);
+			rs = this.stmt.executeQuery(query);
+			/**
+			 * 
+			 * rs = this.stmt.executeQuery("select * from dbo.CustomerRequest"); while
+			 * (rs.next()) { System.out.println(rs.getString("leadId") +
+			 * rs.getString("firstName")); }
+			 */
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+	public int update(String sql) {
+		try {
+			System.out.println("update sql query: " + sql);
+			return this.stmt.executeUpdate(sql);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
 }
