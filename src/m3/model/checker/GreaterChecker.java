ba91a006615f042386676d7cb969546d9025351b
duyuepeng <du.yuep@husky.neu.edu>
2019-12-05 20:24:03 -0800

    fix checkers

---
+++ src/m3/model/checker/GreaterChecker.java
@@ -0,0 +1,9 @@
+package m3.model.checker;
+
+public class GreaterChecker extends Checker<Double> {
+
+    @Override
+    public boolean check(Double o1, Double o2) {
+        return o1 > o2;
+    }
+}
