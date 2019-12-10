6bae311504645b33988457887a942c0fed9fd4b6
Bhoite96 <55061976+Bhoite96@users.noreply.github.com>
2019-12-09 16:23:04 -0800

    Create Vehicle.java

---
+++ src/m2/DealerUI/Vehicle.java
@@ -0,0 +1,127 @@
+package m2.dealerUI;
+
+import java.util.ArrayList;
+
+public class Vehicle {
+    private String id;
+    private String dealerId;
+    private String brand;
+    private String model;
+    private String dateofmanufacturing;
+    private String type;
+    private String category;
+    private String color;
+    private float price;
+    private float mileage;
+    private String flag;
+    
+    Vehicle(){}
+
+	public String getId() {
+	return id;
+}
+
+public void setId(String id) {
+	this.id = id;
+}
+
+public String getDealerId() {
+	return dealerId;
+}
+
+public void setDealerId(String dealerId) {
+	this.dealerId = dealerId;
+}
+
+public String getBrand() {
+	return brand;
+}
+
+public void setBrand(String brand) {
+	this.brand = brand;
+}
+
+public String getModel() {
+	return model;
+}
+
+public void setModel(String model) {
+	this.model = model;
+}
+
+public String getDateofmanufacturing() {
+	return dateofmanufacturing;
+}
+
+public void setDateofmanufacturing(String dateofmanufacturing) {
+	this.dateofmanufacturing = dateofmanufacturing;
+}
+
+public String getType() {
+	return type;
+}
+
+public void setType(String type) {
+	this.type = type;
+}
+
+public String getCategory() {
+	return category;
+}
+
+public void setCategory(String category) {
+	this.category = category;
+}
+
+public String getColor() {
+	return color;
+}
+
+public void setColor(String color) {
+	this.color = color;
+}
+
+public float getPrice() {
+	return price;
+}
+
+public void setPrice(float price) {
+	this.price = price;
+}
+
+public float getMileage() {
+	return mileage;
+}
+
+public void setMileage(float mileage) {
+	this.mileage = mileage;
+}
+
+public String getFlag() {
+	return flag;
+}
+
+public void setFlag(String flag) {
+	this.flag = flag;
+}
+
+	public Vehicle(String carIDString, String dealerIDString, String brandString, String modelString,
+			String dateofmanufacturingString, String typeString, String categoryString,
+			String colorString,float price,  float mileage) {
+		// TODO Auto-generated constructor stub
+		  id = carIDString;
+          dealerId = dealerIDString;
+          brand = brandString;
+          model = modelString;
+         
+          dateofmanufacturing = dateofmanufacturingString;
+          type = typeString;
+          category = categoryString;
+          color = colorString;     
+          this.price = price;
+          this.mileage = mileage;
+
+	}
+	
+
+}
