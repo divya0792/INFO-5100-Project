6b88ffc3625daac78a9efc99221dfca66566e3f0
Yiji Huang <happybridge14@gmail.com>
2019-12-10 02:30:56 -0800

    Clean controller codes

--- src/m1/team14/controller/HomePageController.java
+++ src/m1/team14/controller/HomePageController.java
@@ -2,7 +2,6 @@ package m1.team14.controller;
 import java.util.List;
 import dataproto.Dealer;
 import dataproto.RichText;
-import m1.team14.model.HomePageModel;
 import m1.team2.DealerAllContent;
 import m1.team3.Dealers;
 import m1.team3.login.VMSLoginFrame;
@@ -11,14 +10,6 @@ import m1.DAO.DealerContentDAOImpl;
 import javax.swing.JOptionPane;
 //////////////////////////////
 public class HomePageController extends AbstractController {
-  private HomePageModel homepageModel;
-  public Dealer getInitDealer() {
-    return Dealers.getInstance().getAllDealers().get(0);
-  }
-  public void addModel(HomePageModel model) {
-    super.addModel(model);
-    this.homepageModel = model;
-  }
   public List<Dealer> getDealers() {
     List<Dealer> ret = Dealers.getInstance().getAllDealers();
     return ret;
