cae05797e7bd0b197fd6834818e19c2fee094aec
Yiji Huang <happybridge14@gmail.com>
2019-12-10 16:28:54 -0800

    UPdate login page

--- src/m1/team3/login/LoginService.java
+++ src/m1/team3/login/LoginService.java
@@ -1,12 +1,11 @@
 package m1.team3.login;
 
+import dataproto.Dealer;
+
 public interface LoginService {
 	
 	
 	public boolean validateLogin(String userID, String pwd) ;
 	
-	public boolean rememberMe(String uName) ;
-	
-	public User getUser(String loginID) ;
-
+	public Dealer getDealer(String loginID) ;
 }
