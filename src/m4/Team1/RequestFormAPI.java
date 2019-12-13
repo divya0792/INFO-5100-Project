70842e696cadfb114dc48de3b20bf8e99e72f3a3
Yiji Huang <happybridge14@gmail.com>
2019-12-12 20:28:48 -0800

    Fix close bug for request form

--- src/m4/Team1/RequestFormAPI.java
+++ src/m4/Team1/RequestFormAPI.java
@@ -9,7 +9,7 @@ import m4.Team1.database.dao.VehicleTableDao;
 import m4.Team1.database.utils.Constants;
 
 public class RequestFormAPI {
-	
+
 	public RequestFormAPI(String carID,String dealerID)
 	{
 		RequestFormController controller = new RequestFormController(
@@ -17,14 +17,14 @@ public class RequestFormAPI {
 				new CustomerRequestTableDao(Constants.sqlUrl, Constants.customerRequestTableName));
 		JFrame f = controller.createRequestForm(carID,dealerID);
 		f.setTitle("Customer Request Form");
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		f.setSize(600, 600);
 		f.setVisible(true);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		new RequestFormAPI("PIC2010760","0");
 
 	}
